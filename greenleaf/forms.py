99b48557f4f270b6050aa4b5f88916560e9002f0
aminthika <84611318+aminthika@users.noreply.github.com>
2021-08-18 20:22:52 +0530

    rearranged the form fields

--- greenleaf/forms.py
+++ greenleaf/forms.py
@@ -1,8 +1,8 @@
 from django import forms
 
 class RewildForm(forms.Form):
-    email_address = forms.EmailField()
     number_of_trees = forms.IntegerField()
     location_of_the_trees = forms.CharField()
+    email_address = forms.EmailField()
     
     photo = forms.FileField(label="Upload a photo of one or more of your plants to confirm their existence\n", widget=forms.ClearableFileInput(attrs={'multiple': True}))
