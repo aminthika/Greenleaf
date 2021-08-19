5248a9ca994dd542566e0717fcd39694f9244a9d
aminthika <84611318+aminthika@users.noreply.github.com>
2021-08-19 12:37:02 +0530

    renamed the fields, again.

--- greenleaf/forms.py
+++ greenleaf/forms.py
@@ -1,8 +1,8 @@
 from django import forms
 
 class RewildForm(forms.Form):
-    number_of_trees = forms.IntegerField()
-    location_of_the_trees = forms.CharField()
-    email_address = forms.EmailField()
+    trees = forms.IntegerField()
+    location = forms.CharField()
+    email= forms.EmailField()
     
     photo = forms.FileField(label="Upload a photo of one or more of your plants to confirm their existence\n", widget=forms.ClearableFileInput(attrs={'multiple': True}))
