75b29a0e14cae0007467984f1c224b215ff368a8
aminthika <84611318+aminthika@users.noreply.github.com>
2021-08-19 12:41:23 +0530

    reset the field names and gave them labels instead

--- greenleaf/forms.py
+++ greenleaf/forms.py
@@ -1,8 +1,8 @@
 from django import forms
 
 class RewildForm(forms.Form):
-    trees = forms.IntegerField()
-    location = forms.CharField()
-    email= forms.EmailField()
+    trees = forms.IntegerField(label="number of trees planted")
+    location = forms.CharField(label="location of the trees")
+    email= forms.EmailField(label="email address")
     
     photo = forms.FileField(label="Upload a photo of one or more of your plants to confirm their existence\n", widget=forms.ClearableFileInput(attrs={'multiple': True}))
