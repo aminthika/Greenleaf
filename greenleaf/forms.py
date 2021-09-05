577253df3b8e9f2d785fa919637b6286c418a13e
aminthika <aminthika@raditha.com>
2021-09-05 16:02:23 +0530

    rewild page with progress, form, participants etc

--- greenleaf/forms.py
+++ greenleaf/forms.py
@@ -1,8 +1,9 @@
 from django import forms
 
 class RewildForm(forms.Form):
-    email = forms.EmailField()
-    location = forms.CharField()
-    trees = forms.IntegerField()
-
-    photo = forms.FileField(widget=forms.ClearableFileInput(attrs={'multiple': True}))
+    name = forms.CharField(label="your first and last name")
+    trees = forms.IntegerField(label="number of trees planted")
+    location = forms.CharField(label="location of the trees")
+    email= forms.EmailField(label="email address")
+    
+    photo = forms.FileField(label="Upload a photo of one or more of your plants to confirm their existence\n", widget=forms.ClearableFileInput(attrs={'multiple': True}))
--- greenleaf/views.py
+++ greenleaf/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, reverse, redirect
-
+from django.db.models import Sum
 from greenleaf import forms
 from greenleaf.models import RewildPhoto, Rewild
 
@@ -13,13 +13,20 @@ def home(request):
 def posts(request, slug):
     return render(request, "posts/{0}.html".format(slug))
 
+def rewild_display(request):
+    objects = Rewild.objects.all()
+    total_trees = Rewild.objects.aggregate(Sum('trees'))['trees__sum']
+    goal = 100
+    percentage = total_trees / goal * 100
+    return render(request, "rewild.html", {'rewilds': objects, 'percentage' : percentage, 'goal' : goal, 'total' : total_trees} )
 
 def rewild(request):
     if request.method == 'POST':
         form = forms.RewildForm(request.POST, request.FILES)
         if form.is_valid():
-            rewild = Rewild.objects.create(email=form.cleaned_data['email'],
-                location=form.cleaned_data['location'], trees=form.cleaned_data['trees'])
+            rewild = Rewild.objects.create( name=form.cleaned_data['name'],
+            email=form.cleaned_data['email'], location=form.cleaned_data['location'], 
+            trees=form.cleaned_data['trees']),
 
             for file in request.FILES.getlist('photo'):
                 RewildPhoto.objects.create(rewild=rewild, image=file)
@@ -27,8 +34,11 @@ def rewild(request):
             return redirect(reverse("thank"))   
     else:
         form = forms.RewildForm()
-
-    return render(request, "rewild.html", {'form': form})
+    objects = Rewild.objects.all()
+    total_trees = Rewild.objects.aggregate(Sum('trees'))['trees__sum']
+    goal = 100
+    percentage = total_trees / goal * 100
+    return render(request, "rewild.html", {'form': form, 'rewilds': objects, 'percentage' : percentage, 'goal' : goal, 'total' : total_trees} )
 
 
     
