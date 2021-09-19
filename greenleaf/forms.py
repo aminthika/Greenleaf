13421f1745c17fc55f479d1a1afe0f4c45d22f61
aminthika <aminthika@raditha.com>
2021-09-19 20:27:47 +0530

    displayy names and progress bar

--- greenleaf/forms.py
+++ greenleaf/forms.py
@@ -1,10 +1,7 @@
 from django import forms
 
 class RewildForm(forms.Form):
-<<<<<<< HEAD
     name = forms.CharField(label="your first and last name")
-=======
->>>>>>> 75b29a0e14cae0007467984f1c224b215ff368a8
     trees = forms.IntegerField(label="number of trees planted")
     location = forms.CharField(label="location of the trees")
     email= forms.EmailField(label="email address")
